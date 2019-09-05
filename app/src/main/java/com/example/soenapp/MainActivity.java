8ef925405cb94afa0c688b34ce7054f1e46f9a2f
Kwak04 <tronixdevelop@gmail.com>
2019-09-06 00:30:15 +0900

    feat: added tabs (without function)

--- app/src/main/java/com/example/soenapp/MainActivity.java
+++ app/src/main/java/com/example/soenapp/MainActivity.java
@@ -6,20 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView userImage;
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 사용자 사진 원형 테두리로 표시
         userImage = findViewById(R.id.user_photo);
-
-        // 원형
         userImage.setBackground(new ShapeDrawable(new OvalShape()));
         userImage.setClipToOutline(true);
+
+        tabHost = findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        // First Tab
+        TabHost.TabSpec ts1 = tabHost.newTabSpec("TabSpec1");
+        ts1.setContent(R.id.content1);
+        ts1.setIndicator("내 클럽");
+        tabHost.addTab(ts1);
+
+        // Second Tab
+        TabHost.TabSpec ts2 = tabHost.newTabSpec("TabSpec2");
+        ts2.setContent(R.id.content2);
+        ts2.setIndicator("즐겨찾기");
+        tabHost.addTab(ts2);
     }
 }
