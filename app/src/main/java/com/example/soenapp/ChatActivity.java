93d940a530e43328cd4e1d6c1662f9324d5c63e5
Tronix Kwak <tronixdevelop@gmail.com>
2019-10-04 23:49:08 +0900

    feat: Activate socket communication in chat screen (#43)
    * refactor: ChatMyAdapter -> ChatAdapter
    
    * fix: deleted test codes
    
    * fix(bug): program exits when getSharedPreferences
    
    * style: deleted comment on RecyclerView
    
    * feat: emit message "hi" and log received message

--- app/src/main/java/com/example/soenapp/ChatActivity.java
+++ app/src/main/java/com/example/soenapp/ChatActivity.java
@@ -1,84 +1,120 @@
 package com.example.soenapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class ChatActivity extends AppCompatActivity {
+
+    EditText input;
+    TextView who;
+    Button send;
+
     RecyclerView recyclerView;
     RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager layoutManager;
-    List<Chat> chats;
-    EditText input;
-    TextView who;
-    String my_user_key = "1";
 
     Chat chat;
-    Random random = new Random();
+    List<Chat> chats;
+
+    SharedPreferences pref;
+
+    Socket socket;
+
+    final String TAG = "ChatActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
         recyclerView = findViewById(R.id.chat_recycler_view);
         input = findViewById(R.id.chat_input);
         who = findViewById(R.id.chat_who);
+        send = findViewById(R.id.chat_send);
+
+
+        // Socket Communication
+        Emitter.Listener onConnect = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                socket.emit("clientMessage", "hi");
+            }
+        };
+
+        Emitter.Listener onMessageReceived = new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    JSONObject receivedData = (JSONObject) args[0];
+                    Log.d(TAG, receivedData.getString("msg"));
+                    Log.d(TAG, receivedData.getString("data"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        try {
+            String url = RetrofitService.URL;
+            socket = IO.socket(url);
+            socket.connect();
+            socket.on(Socket.EVENT_CONNECT, onConnect);
+            socket.on("serverMessage", onMessageReceived);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // SharedPreferences
+        pref = getSharedPreferences("userData", MODE_PRIVATE);
+        final String myUserKey = pref.getString("user_key", "");
 
         chats = new ArrayList<>();
 
-        int a;
-
-        for (int i = 0; i < 10 ; i++){
-
-            chat = new Chat();
-            chat.person = String.valueOf(i);
-            chat.text = String.valueOf(i);
-            chat.time = String.valueOf(i);
-
-            a = random.nextInt(3);
-            chat.user_key = String.valueOf(a);
-
-            System.out.println(a);
-
-            chats.add(chat);
-        }
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+        // RecyclerView
         recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new ChatMyAdapter(chats, my_user_key);
+        mAdapter = new ChatAdapter(chats, myUserKey);
         recyclerView.setAdapter(mAdapter);
 
-        findViewById(R.id.chat_send).setOnClickListener(new View.OnClickListener() {
+        send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Chat chat = new Chat();
-                chat.person = "test";
-                chat.time = "test";
-                chat.user_key = "1";
+                chat.person = "none";
+                chat.time = "none";
+                chat.user_key = "none";
 
                 chat.text = input.getText().toString();
                 chats.add(chat);
 
-                for (int i = 0 ; i < chats.size(); i++) {
-                    System.out.println(chats.get(i).toString());
-                }
-                mAdapter = new ChatMyAdapter(chats, my_user_key);
+                mAdapter = new ChatAdapter(chats, myUserKey);
                 recyclerView.setAdapter(mAdapter);
             }
         });
