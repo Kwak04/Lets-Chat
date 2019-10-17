e48195dbaa3e4a6e4483e822be1ad5cbeb2c29ff
Kwak04 <tronixdevelop@gmail.com>
2019-10-17 21:03:34 +0900

    fix: activity_chat => activity_chat_old

--- app/src/main/java/com/example/soenapp/ChatActivity.java
+++ app/src/main/java/com/example/soenapp/ChatActivity.java
@@ -47,7 +47,7 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_chat_old);
 
         recyclerView = findViewById(R.id.chat_recycler_view);
         input = findViewById(R.id.chat_input);
