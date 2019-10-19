c09feb12f6e7273ccf719bb8a9742eeed7edc055
Kwak04 <tronixdevelop@gmail.com>
2019-10-19 19:55:18 +0900

    feat: made gender register screen

--- app/src/main/java/com/example/soenapp/RegisterGenderActivity.java
+++ app/src/main/java/com/example/soenapp/RegisterGenderActivity.java
@@ -1,13 +1,62 @@
 package com.example.soenapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterGenderActivity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    RadioButton male, female;
+    Button next;
+
+    String gender;
+
+    String TAG = "RegisterGenderActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_gender);
+
+        radioGroup = findViewById(R.id.radio_group);
+        male = findViewById(R.id.radiobutton1);
+        female = findViewById(R.id.radiobutton2);
+        next = findViewById(R.id.next);
+
+
+        // 라디오 버튼 누르기 전 다음 버튼 비활성화
+        next.setEnabled(false);
+
+
+        // 성별 선택
+        RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radiobutton1:
+                        gender = "male";
+                        break;
+                    case R.id.radiobutton2:
+                        gender = "female";
+                        break;
+                }
+                Log.d(TAG, "listener: gender=" + gender);
+            }
+        };
+        radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent getIntent = getIntent();
+                String birth = getIntent.getExtras().getString("birth");
+            }
+        });
     }
 }
