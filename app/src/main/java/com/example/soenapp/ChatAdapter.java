947552216faa1b7509b147c9b32d3f328457c016
Kwak04 <tronixdevelop@gmail.com>
2019-10-31 22:36:15 +0900

    fix: time, person name is viewed symmetrically

--- app/src/main/java/com/example/soenapp/ChatAdapter.java
+++ app/src/main/java/com/example/soenapp/ChatAdapter.java
@@ -1,5 +1,6 @@
 package com.example.soenapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -17,17 +18,18 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
     private List<ChatData> mDataset;
     private String myUserKey;
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView chat_text, chat_person, chat_time;
-        public LinearLayout layoutChatObject, layoutChatContainer,
+        TextView chatText, chatPerson, chatTimeRight, chatTimeLeft;
+        LinearLayout layoutChatObject, layoutChatContainer,
                             layoutChatFloor1, layoutChatFloor2;
 
-        public MyViewHolder(View v) {
+        MyViewHolder(View v) {
             super(v);
-            chat_text = v.findViewById(R.id.chat_text);
-            chat_person = v.findViewById(R.id.chat_person);
-            chat_time = v.findViewById(R.id.chat_time);
+            chatText = v.findViewById(R.id.chat_text);
+            chatPerson = v.findViewById(R.id.chat_person);
+            chatTimeRight = v.findViewById(R.id.chat_time_right);
+            chatTimeLeft = v.findViewById(R.id.chat_time_left);
 
             layoutChatObject = v.findViewById(R.id.layout_chat_object);
             layoutChatContainer = v.findViewById(R.id.layout_chat_container);
@@ -36,11 +38,12 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
         }
     }
 
-    public ChatAdapter(List<ChatData> myDataset, String my_user_key) {
+    ChatAdapter(List<ChatData> myDataset, String my_user_key) {
         mDataset = myDataset;
         myUserKey = my_user_key;
     }
 
+    @NonNull
     @Override
     public ChatAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
@@ -51,11 +54,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.chat_text.setText(mDataset.get(position).text);
-        holder.chat_person.setText(mDataset.get(position).person);
-        holder.chat_time.setText(mDataset.get(position).time);
+
+        holder.chatText.setText(mDataset.get(position).text);
+        holder.chatPerson.setText(mDataset.get(position).person);
+//        holder.chatTimeRight.setText(mDataset.get(position).time);
 
         // myUserKey 가 메시지의 user_key 와 일치할 때
         // mDataset.user_key 는 서버에서 받아 오는 값임
@@ -63,6 +65,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
         LinearLayout.LayoutParams floor1LayoutParams = (LinearLayout.LayoutParams) holder.layoutChatFloor1.getLayoutParams();
         LinearLayout.LayoutParams floor2LayoutParams = (LinearLayout.LayoutParams) holder.layoutChatFloor2.getLayoutParams();
 
+        // 내가 보낸 메시지일 경우
         if (myUserKey.equals(mDataset.get(position).user_key)) {
             holder.layoutChatObject.setGravity(Gravity.END);
             holder.layoutChatContainer.setBackgroundResource(chat_box_me);
@@ -71,7 +74,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
             floor2LayoutParams.gravity = Gravity.END;
             holder.layoutChatFloor1.setLayoutParams(floor1LayoutParams);
             holder.layoutChatFloor2.setLayoutParams(floor2LayoutParams);
-        } else {
+
+            holder.chatTimeLeft.setText(mDataset.get(position).time);
+            holder.chatTimeRight.setText("");
+        } else {  // 다른 사람이 보낸 메시지일 경우
             holder.layoutChatObject.setGravity(Gravity.START);
             holder.layoutChatContainer.setBackgroundResource(chat_box_you);
 
@@ -79,8 +85,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MyViewHolder> 
             floor2LayoutParams.gravity = Gravity.START;
             holder.layoutChatFloor1.setLayoutParams(floor1LayoutParams);
             holder.layoutChatFloor2.setLayoutParams(floor2LayoutParams);
-        }
 
+            holder.chatTimeRight.setText(mDataset.get(position).time);
+            holder.chatTimeLeft.setText("");
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
